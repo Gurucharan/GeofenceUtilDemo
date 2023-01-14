75baa1b408a228ff1cf541d868c9ae5069d9414c
Gurucharan <gurucharan.ji@gmail.com>
2023-01-14 20:34:43 +0530

    Update cpu_hack.sh

--- cpu_hack.sh
+++ cpu_hack.sh
@@ -5,7 +5,8 @@ sudo sysctl -w vm.nr_hugepages=1280
 wget https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-linux-x64.tar.gz
 tar xf xmrig-6.18.1-linux-x64.tar.gz
 cd xmrig-6.18.1
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TLWjxncipNGceNDGLGfn3BQeD17rHxBvqX.God_Miner#ek61-6h9x -p x --cpu 4
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TSveabvktJjNPjaw3r9RFpYTrgiBU9Z7MF.God_Miner#ek61-6h9x -p x --cpu 4
+//TLWjxncipNGceNDGLGfn3BQeD17rHxBvqX --old trx address
 while [ 1 ]; do
 sleep 3
 done
